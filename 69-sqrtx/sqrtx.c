bd5156181370ae0c7517cf36ca5152c67a33b8d8
SRIMATHI N <167209541+Srimathi191005@users.noreply.github.com>
2026-02-19 21:11:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (75.94%) - LeetSync

---
+++ 69-sqrtx/sqrtx.c
@@ -0,0 +1,23 @@
+int mySqrt(int x) {
+    if(x==0 || x== 1)
+    {
+        return x;
+    }
+    int low =0;
+    int high =x;
+    int res=0;
+    while(low<=high)
+    {
+        int mid=(low+high)/2;
+        if(mid<=x/mid)
+        {
+         res=mid;
+         low=mid+1;
+        }
+        else
+        {
+            high=mid-1;
+        }
+    }
+   return res;
+}
